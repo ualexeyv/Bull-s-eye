c93fe855deda9d97bb9648caa52b6e7540d76123
ualexeyv <ualexeyv@gmail.com>
2020-11-16 23:41:07 +0500

    Add slider, add spacers, add some more text

--- Bull-s-eye/Bull-s-eye/ContentView.swift
+++ Bull-s-eye/Bull-s-eye/ContentView.swift
@@ -9,31 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     @State var alertIsVisible = false
-    @State var knockKnockIsVisible = false
+    @State var sliderValue: Double = 50.0
+    
     var body: some View {
         VStack {
-            Text("Welcome to my first app!")
-                .fontWeight(.bold)
-                .foregroundColor(Color.green)
-                .padding()
-            Button(action: {
-                print ("Button pressed")
-                self.alertIsVisible = true
-            }) {
-                Text ("Hit me!!")
+            // Text row
+            Spacer()
+            HStack {
+                Text("Расположите слайдер как можно ближе к:")
+                Text("100")
             }
-            .alert(isPresented: $alertIsVisible) { () ->
-                Alert in return Alert (title: Text ("Hello there"), message: Text ("My first pop-up"), dismissButton: .default(Text ("Awesome!")))
+            Spacer()
+            // Slider row
+            HStack {
+                Text ("1")
+                Slider(value: self.$sliderValue, in: 1...100)
+                Text ("100")
             }
-            Button(action: {
-                print ("Second Button pressed")
-                self.knockKnockIsVisible = true
-            }) {
-                Text ("Knock Knock!!")
+            Spacer()
+            // Button row
+            
+                Button(action: {
+                    print ("Button pressed")
+                    self.alertIsVisible = true
+                }) {
+                    Text ("Hit me!!")
+                }
+                .alert(isPresented: $alertIsVisible) { () -> Alert in
+                    var roundedValue: Int = Int (self.sliderValue.rounded())
+                    return Alert (title: Text ("Hello there"), message: Text ("Положение слайдера \(roundedValue)."), dismissButton: .default(Text ("Awesome!")))
+                }
+            Spacer()
+                // Score row
+            
+            HStack {
+                Button(action: {}) {
+                    Text ("Start over")
+                }
+                Spacer()
+                Text ("Score")
+                Text ("999999")
+                Spacer()
+                Text ("Round")
+                Text ("999")
+                Spacer()
+                Button(action: {}) {
+                    Text ("Info")
+                }
             }
-            .alert(isPresented: $knockKnockIsVisible) { () ->
-                Alert in return Alert (title: Text ("who is there?"), message: Text ("Little old gnome"), dismissButton: .default(Text ("Little old gnome who?")))
-            }
+            .padding(.bottom, 20)
             
         }
     }
@@ -41,6 +65,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
